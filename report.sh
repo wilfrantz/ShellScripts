50976ced521f9a49b2152c98fffaa0fda3886151
Wilfrantz Dede <wilfrantz@outlook.com>
2020-11-20 19:26:59 -0500

    Improved script flow

--- report.sh
+++ report.sh
@@ -6,24 +6,28 @@
 # set -x # Debug mode.
 # set -n # Run w/o execution.
 
-### Variable ###
-OutPutFile="$HOME"/reports/report_$(date | sed s/" "/_/g).txt
-Ram_eater=$(ps aux | awk '{print $6 "\t" $11}' | sort -rn | head -25 | \
+# NOTE  alias upsys='apt-get update && apt-get upgrade -y' in ~/.zshrc
+
+write_to_file(){
+	local Ram_eater=$(ps aux | awk '{print $6 "\t" $11}' | sort -rn | head -25 | \
 	awk '{print $1/1024 " MB\t\t" $2}') # >> "$OutPutFile"
-# alias upsys='apt-get update && apt-get upgrade -y' # NOTE in ~/.zshrc
 
-# printf "\nReport for %s: \n\n" "$(hostname)" > "$OutPutFile"
-echo "Report for '$(hostname)" > "$OutPutFile"
-upsys | grep -i "upgraded" | sed s/", "/\\n/g >> "$OutPutFile" 
+	local OutPutFile="$HOME"/reports/report_$(date | sed s/" "/_/g).txt
 
-printf "\n\n Ram eater Aplications: \n" >> "$OutPutFile"
-"$Ram_eater" >> "$OutPuFile"
+	# printf "\nReport for %s: \n\n" "$(hostname)" > "$OutPutFile"
+	echo "Report for '$(hostname)" 
+	upsys | grep -i "upgraded" | sed s/", "/\\n/g  
 
-#ps aux | awk '{print $6 "\t" $11}' | sort -rn | head -25 | \
-	#awk '{print $1/1024 " MB\t\t" $2}' >> "$OutPutFile"
+	printf "\n\n Ram eater Aplications: \n" 
+	"$Ram_eater" >> "$OutPuFile"
 
-printf "\nRootkit check... \n" >> "$OutPutFile"
-sudo chkrootkit | grep -i "infected" >> "$OutPutFile"
+	printf "\nRootkit check... \n" 
+	sudo chkrootkit | grep -i "infected" 
+
+} >> "$OutPutFile"
+
+
+write_to_file()
 
 telegram-send --file "$OutPutFile" --caption "Report $(date)"
 
