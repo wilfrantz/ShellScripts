3a0ff76cb9baf89e9a88e9fce0aca2cdebc64057
Wilfrantz Dede <wilfrantz@outlook.com>
2020-12-13 14:30:40 -0500

    Uses xargs

---
+++ run/.bash_aliases
@@ -0,0 +1,60 @@
+
+
+########################################################
+# Aliases
+########################################################
+
+# reload zsh config
+alias reload!='RELOAD=1 source ~/.zshrc'
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+	colorflag="--color"
+else # macOS `ls`
+	colorflag="-G"
+fi
+
+# Tree command alias.
+alias t='tree'
+
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+#alias ls='ls -l | lolcat'
+
+alias l="ls -lah ${colorflag}"
+alias la="ls -AF ${colorflag}"
+alias ll="ls -lFh ${colorflag}"
+alias lld="ls -l | grep ^d"
+alias rmf="rm -rf"
+
+# Helpers
+alias grep='grep --color=auto'
+alias df='df -h' # disk free, in Gigabytes, not bytes
+alias du='du -h -c' # calculate disk usage for a folder
+
+# Applications
+alias ios='open -a /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+# remove broken symlinks
+alias clsym="find -L . -name . -o -type d -prune -o -type l -exec rm {} +"
+
+# git aliases
+alias gs='git s'
+alias glog="git l"
+
+# tmux aliases
+alias ta='tmux attach'
+alias tls='tmux ls'
+alias tat='tmux attach -t'
+alias tns='tmux new-session -s'
---
+++ run/run.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# set -x # Run in debug mode.
+# set -n # Run w\o execution.
+
+kali(){
+	{
+		echo '
+				"preload"
+				"gdb"
+				"cgdb"
+				"bleachbit"
+				"zsh"
+				"neofetch"
+				"bum"
+				"gnome-do"
+				"bum"
+				#"figlet"
+				"shutter"
+				"scrub"
+				"apt-file"
+				"cmatrix"
+				"lazy"
+				"tree"
+				"speedtest-cli"
+				"cdebootstrap"
+				"live-build"
+				"git"
+				"curl"
+				"dnsutils"
+			'
+	} | xargs apt --assume-yes install 2> error.log 1> /dev/null
+
+}
+
+
+getUtils(){
+	local source_url="https://dede.dev/run/"
+	rc_files=(".zshrc", ".bashrc", ".vimrc", ".bash_aliases", "clean", ".screenrc")
+
+        for rcFile in "${rc_files[@]}"
+            do
+                wget "$source_url""$rcFile" -o ~/."$rcFile" &> /dev/null
+            done
+
+        # Installing Oh My Zsh
+        #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+        # Install PowerLeve10K theme
+        #git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+        # get Nerd fonts
+        #wget "$source_url""Fira\ Mono\ Regular\ Nerd\ Font\ Complete.otf"
+        #wget "$source_url""Fura\ Mono\ Regular\ Nerd\ Font\ Complete.otf"
+
+        # Autosuggestion 
+        git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+        # syntax highlighting
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+}
+
+#main 
+{
+	SYSTEM=$(uname -a | cut -d" " -f1)
+	if [ "$SYSTEM" = "kali" ]; then
+			kali && getUtils 
+		else 
+			 getUtils 
+	fi
+# TODO:curl http://j.mp/spf13-vim3 -L -o - | sh
+} && source ~/.bashrc
