d93f34dc4fc36dab05216059af4f1ef9503e3df1
Wilfrantz Dede <wilfrantz@outlook.com>
2020-12-23 20:58:33 -0500

    typos

--- run/setup.sh
+++ run/setup.sh
@@ -11,14 +11,14 @@ SYSTEM=$(uname -a | cut -d" " -f1)
 source_url="https://dede.dev/run/"
 
 command_exists() {
-    local ret='0'
-    command -v $1 > /dev/null 2>&1 { local ret='1';}
-
-    # fail on non-zero return value
-    if [ "$ret" -ne '0' ]; then 
-            return 1
-    fi
-   return 0
+   local ret='0'
+   command -v "$1" > /dev/null 2>&1 { local "$ret"='1';}
+   # fail on non-zero return value
+   if [ "$ret" -ne '0' ]; then 
+      return 1
+   else
+      return 0
+   fi
 }
 
     # install ZSH shell on your OS 
