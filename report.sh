e9c159948e86518d7a19fa19fa92d3436b800be6
wilfrantz Dede <wilfrantz@outlook.com>
2021-09-18 17:53:04 -0400

    Fixes minox syntax issues

--- report.sh
+++ report.sh
@@ -9,17 +9,19 @@
 # NOTE  alias upsys='apt-get update && apt-get upgrade -y' in ~/.zshrc
 
 write_to_file(){
-	local Ram_eater=$(ps aux | awk '{print $6 "\t" $11}' | sort -rn | head -25 | \
+	local Ram_eater
+	Ram_eater=$(ps aux | awk '{print $6 "\t" $11}' | sort -rn | head -25 | \
 	awk '{print $1/1024 " MB\t\t" $2}') # >> "$OutPutFile"
 
-	local OutPutFile="$HOME"/reports/report_$(date | sed s/" "/_/g).txt
+	local OutPutFile
+	OutPutFile="$HOME"/reports/report_$(date | sed s/" "/_/g).txt
 
 	# printf "\nReport for %s: \n\n" "$(hostname)" > "$OutPutFile"
 	echo "Report for '$(hostname)" 
 	upsys | grep -i "upgraded" | sed s/", "/\\n/g  
 
 	printf "\n\n Ram eater Aplications: \n" 
-	"$Ram_eater" >> "$OutPuFile"
+	"$Ram_eater" >> "$OutPutFile"
 
 	printf "\nRootkit check... \n" 
 	sudo chkrootkit | grep -i "infected" 
