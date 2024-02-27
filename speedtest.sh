c0800d753568fdfc959f21ccc1efcc1e354a8c4e
wilfrantz Dede <wilfrantz@outlook.com>
2024-02-27 16:12:02 -0500

    Uses Telegram only.

--- speedtest.sh
+++ speedtest.sh
@@ -1,40 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# This Bash script automates internet speed testing and send reporting to a designated system administrator.
-
+# This Bash script automates internet speed testing and sends reporting to a designated system administrator.
+#
+# source the configuration file 
+#source ~/.zshrc
 
 # Check if speedtest-cli is installed
-if ! command -v speedtest-cli &> /dev/null
-then
+if ! command -v speedtest-cli &> /dev/null; then
     echo "speedtest-cli is not installed. Please install it using 'pip install speedtest-cli'."
     exit 1
 fi
 
-
 # Read Telegram bot token and chat ID from environment variables
 TOKEN="$TELEGRAM_BOT_TOKEN"
 CHAT_ID="$TELEGRAM_CHAT_ID"
+DEVICE=$(hostname -f)
 
 # Perform speedtest and capture the result
 speedtestResult=$(speedtest-cli --simple)
 
-# Extract download and upload speeds
-downloadSpeed=$(echo "$speedtestResult" | awk '/Download:/ {print $2}')
-uploadSpeed=$(echo "$speedtestResult" | awk '/Upload:/ {print $2}')
-
-
-# Email configuration
-recipient="contact@dede.dev"
-subject="Internet Speed Test: "
-body="Download Speed: $downloadSpeed\nUpload Speed: $uploadSpeed"
-
-# Send email
-echo -e "$body" | mailx -s "$subject" "$recipient"
+# TODO Extract download and upload speeds to process further if needed
+# downloadSpeed=$(echo "$speedtestResult" | awk '/Download:/ {print $2}')
+# uploadSpeed=$(echo "$speedtestResult" | awk '/Upload:/ {print $2}')
 
 # Prepare message
-message="Home Internet Speed : 
-Download Speed: $downloadSpeed
-Upload Speed: $uploadSpeed"
+message="$DEVICE Internet Speed:
+$speedtestResult"
 
 # Send message via Telegram
 curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$message" > /dev/null
+
