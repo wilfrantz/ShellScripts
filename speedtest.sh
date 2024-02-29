a1422e801e6a149fb14e41d349c8a07006b7289b
wilfrantz Dede <wilfrantz@outlook.com>
2024-02-28 20:07:04 -0500

    both standard output and standard error are redirected to the same variable speedtestResult.

--- speedtest.sh
+++ speedtest.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 #
 # This Bash script automates internet speed testing and sends reporting to a designated system administrator.
-#
-# source the configuration file 
-#source ~/.zshrc
 
 # Check if speedtest-cli is installed
 if ! command -v speedtest-cli &> /dev/null; then
-    echo "speedtest-cli is not installed. Please install it using 'pip install speedtest-cli'."
-    exit 1
+	echo "speedtest-cli is not installed. Please install it using 'pip install speedtest-cli'."
+	exit 1
 fi
+
+
+# Read the content of the file
+speedtestResult=$(speedtest-cli --simple 2>&1)
 
 # Read Telegram bot token and chat ID from environment variables
 TOKEN="$TELEGRAM_BOT_TOKEN"
 CHAT_ID="$TELEGRAM_CHAT_ID"
+
 DEVICE=$(hostname -f)
-
-# Perform speedtest and capture the result
-speedtestResult=$(speedtest-cli --simple)
-
-# TODO Extract download and upload speeds to process further if needed
-# downloadSpeed=$(echo "$speedtestResult" | awk '/Download:/ {print $2}')
-# uploadSpeed=$(echo "$speedtestResult" | awk '/Upload:/ {print $2}')
 
 # Prepare message
 message="$DEVICE Internet Speed:
