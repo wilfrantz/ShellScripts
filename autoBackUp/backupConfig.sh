9b22b55e8282d7928aeeedd2bae11bcaa2255751
Dede <contact@dede.dev>
2025-03-15 01:14:27 -0400

    Adds critical directories and data to ensure a complete and restorable backup

--- autoBackUp/backupConfig.sh
+++ autoBackUp/backupConfig.sh
@@ -4,4 +4,4 @@
 export backupDir="/path/to/backup" 
 export rcloneRemote="gdrive_backup:" # or "gdrive_backup:" if you do not use crypt
 export rcloneCryptRemote="gdrive_encrypted:" # rclone encrypted remote
-export dataToBackup="/etc /home " # Add data 
+export dataToBackup="/etc /home /var /opt /srv /root " # Add data 
